db6ecbcbcfc9a5cce62fd11f97417f9d8156514a
Carl Chan <carl.chan@zuhlke.com>
2020-09-05 12:30:23 +0800

    Fix location parsing.

--- routesSpider/spiders/routes_spider.py
+++ routesSpider/spiders/routes_spider.py
@@ -3,6 +3,7 @@ import math
 import urllib.parse as urlparse
 from urllib.parse import parse_qs
 from mergedeep import merge
+from decimal import *
 
 
 class QuotesSpider(scrapy.Spider):
@@ -12,6 +13,7 @@ class QuotesSpider(scrapy.Spider):
         # 'http://www.walkonhill.com/route.php?area=1&seq=2',
         # http://www.walkonhill.com/route_en.php?area=1&seq=1 // following link?
     ]
+    trail_id = 13
 # TODO: DUPEFILTER_CLASS, aviod hitting server too quickly
 
     def parse(self, response):
@@ -33,84 +35,80 @@ class QuotesSpider(scrapy.Spider):
         generalInfo = response.css(
             'div.generalInfo td.right_td::text').getall()
         trail = {
+            'id': self.trail_id,
             'name': generalInfo[0],
             'name_en': 'XXX',
             'description': response.css('p.intro::text').get(),
             'description_en': 'XXX',
-            'regions': [{
-                'name': areaToRegion[area],
-                'name_en': 'XXX TODO'
-            }],
-            'districts': [{
-                'name': response.css('p#indicator::text').get().split('\\')[2].strip(),
-                'name_en': 'XXX TODO'
-            }],
+            'regions': [
+                {
+                    'name': areaToRegion[area],
+                    'name_en': 'XXX TODO'
+                }
+            ],
+            'districts': [
+                {
+                    'name': response.css('p#indicator::text').get().split('\\')[2].strip(),
+                    'name_en': 'XXX TODO'
+                }
+            ],
             'height': 9999,  # TODO
-            'distanceInKm': generalInfo[1].replace(' 公里', '').replace('.0', ''),
+            # FIXME: trim .0
+            'distanceInKm': float(generalInfo[1].replace(' 公里', '')),
             'difficulty': math.ceil(float(response.css('p.current_rating::text').getall()[0])),
-            'durationInHour': generalInfo[2].replace(' 小時', ''),
+            'durationInHour': float(generalInfo[2].replace(' 小時', '')),
             'sceneRating': math.ceil(float(response.css('p.current_rating::text').getall()[1])),
             'recommendRating': 3,
             'route': {
                 'starts': [
                     {
-                        'name': 'TODO',
-                        'name_en': 'TODO',
+                        'location': 'TODO',
+                        'location_en': 'TODO',
                         'description': response.css('div#tab-1 p::text')[0].get().strip(),
-                        'description_en': 'TODO',
-                        'marker': {
-                            'latitude': 22.281084,
-                            'longitude': 114.160931
-                        }
+                        'description_en': 'XXX',
                     }
                 ],
                 'paths': [
                     {
-                        'name': 'TODO',
-                        'name_en': 'TODO',
-                        'description': location.strip(),
+                        'location': location.strip(),
+                        'name_en': 'XXX',
+                        'description': 'TODO',
                         'description_en': 'TODO',
-                        'marker': {
-                            'latitude': 22.281084,
-                            'longitude': 114.160931
-                        }
                     } for location in paths
                 ],
                 'ends': [
                     {
-                        'name': 'TODO',
-                        'name_en': 'TODO',
+                        'location': 'TODO',
+                        'location_en': 'TODO',
                         'description': response.css('div#tab-1 p::text')[1].get().strip(),
-                        'description_en': 'TODO',
-                        'marker': {
-                            'latitude': 22.281084,
-                            'longitude': 114.160931
-                        }
+                        'description_en': 'XXX',
                     }
                 ]
             },
             "images": [
                 {
-                    "url": "TODO",
+                    "url": f'assets/images/{self.trail_id}-001.jpg',
                     "credit": "TODO",
                     "sourceUrl": "TODO"
                 }
             ],
             "map": {
-                "geoJson": "assets/geojson/lion-rock.geojson",  # TODO
+                "geoJson": f'assets/geojson/{self.trail_id}.geojson',
                 "zoom": 14.84,
                 "center": {
                     "latitude": 22.352484,
                     "longitude": 114.185105
                 }
             },
-            "reference": url
+            "reference": [url]
         }
         englishUrl = url.replace('route.php', 'route_en.php')
         englishPageRequest = scrapy.Request(
             englishUrl, callback=self.parse_english_page)
         englishPageRequest.meta['trail'] = trail
         englishPageRequest.meta['area'] = area
+
+        self.trail_id += 1
         yield englishPageRequest
 
     def parse_english_page(self, response):
@@ -138,7 +136,27 @@ class QuotesSpider(scrapy.Spider):
         district = {
             'name_en': response.css('p#indicator::text').get().split('\\')[2].strip(),
         }
+        startPath = {
+            'name_en': 'TODO',
+            'description_en': response.css('div#tab-1 p::text')[0].get().strip(),
+        }
+        endPath = {
+            'name_en': 'TODO',
+            'description_en': response.css('div#tab-1 p::text')[1].get().strip(),
+        }
+        paths = response.css('div#trackpointList ol li a::text').getall()
+        pathList = [
+            {
+                'location_en': location.strip(),
+                'description_en': 'TODO',
+            } for location in paths
+        ]
         trail = response.meta['trail']
-        trail['regions'] = merge(trail['regions'][0], region)
-        trail['districts'] = merge(trail['districts'][0], district)
+        trail['regions'][0] = merge(trail['regions'][0], region)
+        trail['districts'][0] = merge(trail['districts'][0], district)
+        trail['route']['starts'][0] = merge(
+            trail['route']['starts'][0], startPath)
+        trail['route']['ends'][0] = merge(trail['route']['ends'][0], endPath)
+        for i, location in enumerate(trail['route']['paths']):
+            location = merge(location, pathList[i])
         yield merge(trail, englishTrail)
